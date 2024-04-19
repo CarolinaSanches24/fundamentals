587daac39a922ec0759d7f50a2c640a2dc4e439b
CAROL SANCHES <135077050+CarolinaSanches24@users.noreply.github.com>
2024-04-19 17:07:38 -0300

    save

--- src/resolvers/users-resolvers.ts
+++ src/resolvers/users-resolvers.ts
@@ -1,15 +1,14 @@
 import { Resolver, Query, Mutation, Arg } from "type-graphql";
 import { User } from "../dtos/models/users.model";
 import { UserInput } from "../dtos/inputs/userInput";
-import { db } from "../infra/dataBase/mysql/db";
-import { schemaUser } from "../infra/dataBase/schemas/userSchema";
+
 
 @Resolver(() => User)
 export class UsersResolver {
   @Query(() => [User])
   @Mutation(() => User)
   async createUser(@Arg("data") data: UserInput) {
-    const user = await db.insert(schemaUser).values(data);
-    return user;
+    
+    
   }
 }
